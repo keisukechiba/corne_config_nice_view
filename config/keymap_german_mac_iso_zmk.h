6430d7656b335c174375637791c1823325fe49d6
keisukechiba <31137144+keisukechiba@users.noreply.github.com>
2024-02-23 13:51:43 +0900

    Update keymap_german_mac_iso_zmk.h

--- config/keymap_german_mac_iso_zmk.h
+++ config/keymap_german_mac_iso_zmk.h
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         };
     };
 // }}}
-  };
+
 // #pragma once
 
 #define XXX &none
@@ -387,6 +387,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_LN_CARET &kp RA(APOS) // ^
 #define DE_LN_DQUO &kp LS(N2) // "
 
+
+  };
 // ###############
 // #define DE_FSLH &kp AMPS  // "/" DE_SLSH
 // #define DE_STAR &kp RBRC  // "*" DE_ASTR

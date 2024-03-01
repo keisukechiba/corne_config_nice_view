29acbb0a406e3d6f3abd17861b5d3d621e48f4ed
keisukechiba <31137144+keisukechiba@users.noreply.github.com>
2024-03-01 11:28:16 +0900

    Update keymap_german_mac_iso_zmk.h

--- config/keymap_german_mac_iso_zmk.h
+++ config/keymap_german_mac_iso_zmk.h
@@ -247,7 +247,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DE_RABK &kp GREATER_THAN // >
 #define DE_SCLN &kp LS(COMMA) // ;
 #define DE_COLN &kp LS(DOT)  // :
-#define DE_UNDS &gqt EQUAL AT // _ ?
+#define DE_UNDS &gqt HASH EXCLAMATION // _ ?
 
 /* Alted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
